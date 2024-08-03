9953d6eccf23b02eb78a661e8ea486b8f124fe97
Jennifer Shendre <105356506+Jenny-shendre@users.noreply.github.com>
2024-08-04 02:52:41 +0530

    Delete Controllers directory

--- Controllers/homeController.js
+++
@@ -1,7 +0,0 @@
-import asyncHandler from '../utils/asyncHandler.js';
-
-export const welcomeToROF = asyncHandler(async (req, res) => {
-   return res.json( {msg:"WELCOME TO ROF"} )
-});
-
-export default welcomeToROF
